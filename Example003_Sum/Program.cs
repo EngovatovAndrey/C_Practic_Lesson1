fa7b9ddae658dbc5610234fa45ce856b0ff0b60b
Andrey Engo <5522662@gmail.com>
2022-12-15 00:12:41 +0300

    Test Magic on Exemple003

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,6 +1,6 @@
 ﻿int numberA = new Random().Next(1, 10); // 1 2 3 4..... до 9 включительно
-Console.WriteLine(numberA);
+Console.Write(numberA);
 int numberB = new Random().Next(1, 10);
-Console.WriteLine(numberB);
+Console.Write(numberB);
 int sum = numberA + numberB;
 Console.WriteLine(sum);
